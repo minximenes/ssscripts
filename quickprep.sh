eaa504a05ee6029e9caa44a5b97a529259bc17d8
unknown <min.ximenes@gmail.com>
2024-05-30 12:35:43 +0800

    add

---
+++ quickprep.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# receive paras
+PORT=$([ -n "$1" ] && echo $1 || echo 9541)
+PWD=$([ -n "$2" ] && echo "$2" || echo "c4dF-")
+
+# install
+if dpkg -s shadowsocks-libev | grep "installed" >/dev/null 2>&1; then
+    echo "The shadowsocks-libev already exists"
+else
+    sudo apt-get update
+    sudo apt-get install -y shadowsocks-libev=3.3.5+ds-10build3
+    if [ $? -eq 0 ]; then
+        echo "Finish installing shadowsocks-libev"
+    fi
+fi
+# create conf file
+cat << EOF > $PORT.tmp
+{
+    "server":["::0", "0.0.0.0"],
+    "mode":"tcp_and_udp",
+    "server_port":$PORT,
+    "password":"$PWD",
+    "timeout":86400,
+    "method":"chacha20-ietf-poly1305",
+    "fast_open":false
+}
+EOF
+sudo mv ./$PORT.tmp /etc/shadowsocks-libev/$PORT.json
+# start service
+sudo systemctl enable shadowsocks-libev-server@$PORT.service --now
+sudo systemctl status shadowsocks-libev-server@$PORT.service
+
+echo "Please login with port $PORT and passcode $PWD and AEAD chacha20-ietf-poly1305"
+exit 0
